be3b321205cf7de7a89251a1ae04ad9409e7a2f2
Mikhail Kortikov <kortikov.m.r@gmail.com>
2024-10-17 23:25:43 +0300

    Adaptive View

---
+++ AdaptiveView/RectFullPositionByState.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace AppStructure.AdaptiveView
+{
+    public class RectFullPositionByState : RectFullPosition
+    {
+        [SerializeField] private RectPositionByState _rectPositionByState;
+
+        public void Initialize(RectPositionByState rectPositionByState)
+        {
+            base.Initialize(rectPositionByState.RectTransform);
+            _rectPositionByState = rectPositionByState;
+        }
+        
+        public override bool Apply<T>(T obj)
+        {
+            var isType = typeof(T) == typeof(int);
+            if (isType)
+            {
+                var index = (int)(object)obj;
+                base.Apply(_rectPositionByState.RectTransform);
+                _rectPositionByState.ApplyPosition(index);
+            }
+            return isType;
+        }
+    }
+}
---
+++ AdaptiveView/RectPositionByState.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AYellowpaper.SerializedCollections;
+using DingoUnityExtensions.MonoBehaviours.UI;
+using NaughtyAttributes;
+using TMPro;
+using UnityEngine;
+
+namespace AppStructure.AdaptiveView
+{
+    public class RectPositionByState : UIBehaviour
+    {
+        [SerializeField, Dropdown(nameof(States)), Tooltip("If index == -1, then bake new state.")]
+        private int _indexForBake = -1;
+        
+        [SerializeField] private SerializedDictionary<RectTransform, FullPosList> _rectPositions = new();
+        [SerializeField] private List<AdaptByStateElement> _adaptByStateElements = new();
+        
+        private List<int> States => Enumerable.Empty<int>().Append(-1).Concat(Enumerable.Range(0, MaxCount)).ToList();
+        private int MaxCount
+        {
+            get
+            {
+                var firstOrDefault = _rectPositions.Where(p => p.Value != null).OrderBy(e => e.Value.Count).FirstOrDefault();
+                if (firstOrDefault.Value == default)
+                    return 0;
+                return firstOrDefault.Value.Count;
+            }
+        }
+
+        public void ApplyPosition(int index)
+        {
+            foreach (var (rt, values) in _rectPositions)
+            {
+                if (rt == null || values == null || values.Count == 0)
+                    return;
+                var i = Math.Min(index, values.Count - 1);
+                var fullPos = values.RectFullPositions[i];
+
+                if (rt.TryGetComponent<TMP_Text>(out var text))
+                    fullPos.Apply(text);
+                else
+                    fullPos.Apply(rt);
+            }
+
+            foreach (var adaptByStateElement in _adaptByStateElements)
+            {
+                adaptByStateElement.Adapt(index);
+            }
+        }
+        
+        private void ChangeBakedState()
+        {
+            foreach (var (rt, values) in _rectPositions.ToList())
+            {
+                RectFullPosition rectFullPos;
+                if (rt.TryGetComponent<TMP_Text>(out var text))
+                    rectFullPos = ScriptableObject.CreateInstance<TextFullPosition>().Initialize(text);
+                else
+                    rectFullPos = ScriptableObject.CreateInstance<RectFullPosition>().Initialize(rt);
+
+                var index = _indexForBake;
+                if (values.Count == 0)
+                {
+                    values.RectFullPositions.Add(rectFullPos);
+                }
+                else if (index < 0)
+                {
+                    values.RectFullPositions.Add(rectFullPos);
+                }
+                else if (index >= values.Count)
+                {
+                    values.RectFullPositions.AddRange(Enumerable.Repeat(ScriptableObject.CreateInstance<RectFullPosition>(), index - values.Count));
+                    values.RectFullPositions.Add(rectFullPos);
+                }
+                else
+                {
+                    var prevFullPos = values.RectFullPositions[index];
+                    rectFullPos.IgnoreActiveBake = prevFullPos.IgnoreActiveBake;
+                    values.RectFullPositions[index] = rectFullPos;
+                }
+                
+                _rectPositions[rt] = values; // Addictive Serialization handle
+            }
+
+            foreach (var adaptByStateElement in _adaptByStateElements)
+            {
+                adaptByStateElement.BakeForIndex(_indexForBake);
+            }
+        }
+
+        public void BakeForState(int index)
+        {
+            _indexForBake = index;
+            ChangeBakedState();
+        }
+        
+        [Button]
+        private void BakeState() => ChangeBakedState();
+    }
+}
