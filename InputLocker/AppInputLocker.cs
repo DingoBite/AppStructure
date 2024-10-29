0474de83eb015574f34ac57ce2a99dd097901a64
Mikhail Kortikov <kortikov.m.r@gmail.com>
2024-10-30 00:56:47 +0300

    AppInput locker

---
+++ InputLocker/AppInputLocker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace AppStructure.InputLocker
+{
+    public abstract class AppInputLocker<TLockMessage> : MonoBehaviour, IAppInputLocker<TLockMessage>
+    {
+        protected TLockMessage LastLockMessage { get; private set; }
+        protected long LockFlags { get; private set; }
+
+        public void Initialize()
+        {
+            LockFlags = 0;
+            Disable();
+        }
+
+        public void Enable(TLockMessage lockMessage, ushort lockFlag = 0)
+        {
+            LastLockMessage = lockMessage;
+            LockFlags |= (uint)(1 << lockFlag);
+        }
+
+        public void Disable(ushort lockFlag = 0)
+        {
+            LockFlags &= ~(uint)(1 << lockFlag);
+            if (LockFlags != 0)
+                return;
+        }
+
+        protected abstract void OnLockEnable(TLockMessage lockMessage);
+        protected abstract void OnLockDisable();
+    }
+}
---
+++ InputLocker/IAppInputLocker.cs
@@ -0,0 +1,8 @@
+﻿namespace AppStructure.InputLocker
+{
+    public interface IAppInputLocker<in TLockMessage>
+    {
+        void Enable(TLockMessage lockMessage, ushort lockFlag);
+        void Disable(ushort lockFlag = 0);
+    }
+}
