bd350b829d7e53f9be2ca5d02cf286657d3a7bbc
Mikhail Kortikov <kortikov.m.r@gmail.com>
2024-12-05 17:14:59 +0300

    Subscription after Binding

--- BaseElements/StateViewElement.cs
+++ BaseElements/StateViewElement.cs
@@ -6,11 +6,24 @@ namespace AppStructure.BaseElements
 {
     public abstract class StateViewElement<TState, TAppModel, TAppConfig> : SubscribableBehaviour where TState : Enum
     {
+        private bool _binded;
+        protected sealed override bool ForSubscriptionPrepared => _binded;
+
         public virtual void SetDefaultValues() {}
         
         public virtual void PreInitialize() {}
         public virtual Task InitializeAsync(TAppConfig appConfig) => Task.CompletedTask;
-        public virtual Task BindAsync(TAppModel appModel) => Task.CompletedTask;
+        public virtual Task BindAsync(TAppModel appModel)
+        {
+            _binded = true;
+            if (isActiveAndEnabled)
+            {
+                UnsubscribeOnly();
+                SubscribeOnly();
+            }
+            return Task.CompletedTask;
+        }
+
         public virtual Task PostInitializeAsync() => Task.CompletedTask;
 
         public abstract Task EnableElementAsync(TransferInfo<TState> transferInfo);
--- Utils/SubscribableBehaviour.cs
+++ Utils/SubscribableBehaviour.cs
@@ -4,17 +4,25 @@ namespace AppStructure.Utils
 {
     public abstract class SubscribableBehaviour : MonoBehaviour
     {
+        protected virtual bool ForSubscriptionPrepared => true;
+        
         protected abstract void SubscribeOnly();
         protected abstract void UnsubscribeOnly();
 
         protected virtual void OnEnable()
         {
+            if (!ForSubscriptionPrepared)
+                return;
+            
             UnsubscribeOnly();
             SubscribeOnly();
         }
 
         protected virtual void OnDisable()
         {
+            if (!ForSubscriptionPrepared)
+                return;
+            
             UnsubscribeOnly();
         }
     }
