c8fc3f1468c6498f522515fe3b7fbb9742189876
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-01-24 19:03:04 +0300

    Add StateBased Subscription support

--- BaseElements/StateViewElement.cs
+++ BaseElements/StateViewElement.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using System.Threading.Tasks;
 using AppStructure.Utils;
+using UnityEngine;
 
 namespace AppStructure.BaseElements
 {
     public abstract class StateViewElement<TState, TAppModel, TAppConfig> : SubscribableBehaviour where TState : Enum
     {
-        private bool _binded;
-        protected sealed override bool ForSubscriptionPrepared => _binded;
-
-        public virtual void SetDefaultValues() {}
+        [SerializeField] private bool _enableDisableFromState = true;
         
+        private bool _binded;
+        protected sealed override bool ForSubscriptionPrepared => !_enableDisableFromState && _binded;
+
         public virtual void PreInitialize() {}
         public virtual Task InitializeAsync(TAppConfig appConfig) => Task.CompletedTask;
         public virtual Task BindAsync(TAppModel appModel)
         {
             _binded = true;
-            if (isActiveAndEnabled)
+            if (ForSubscriptionPrepared && isActiveAndEnabled)
             {
                 UnsubscribeOnly();
                 SubscribeOnly();
@@ -27,9 +28,23 @@ namespace AppStructure.BaseElements
         public virtual Task PostInitializeAsync() => Task.CompletedTask;
 
         public abstract Task EnableElementAsync(TransferInfo<TState> transferInfo);
-        public abstract void OnStartScreenEnable();
 
-        public abstract Task DisableElementAsync(TransferInfo<TState> transferInfo);
+        public virtual void OnStartScreenEnable()
+        {
+            if (_enableDisableFromState)
+            {
+                UnsubscribeOnly();
+                SubscribeOnly();
+            }
+        }
+
+        public virtual Task DisableElementAsync(TransferInfo<TState> transferInfo)
+        {
+            if (_enableDisableFromState)
+                UnsubscribeOnly();
+            return Task.CompletedTask;
+        }
+        
         public abstract void OnCompletelyDisable();
         
         protected override void SubscribeOnly() {}
